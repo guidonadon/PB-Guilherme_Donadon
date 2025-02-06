d1581e515acdf6dfa790b1077602a3c0730e8f26
Guilherme Donadon <guilhermedonadon@outlook.com>
2025-02-06 16:24:08 -0300

    Add files via upload

---
+++ Sprint_03/Exercicios/Exercicios_Python_1/EX01.py
@@ -0,0 +1,5 @@
+nome = "Guilherme"
+idade = 26
+anoNasc = int(2024 - idade)
+centenario = int(anoNasc + 100)
+print(centenario)
---
+++ Sprint_03/Exercicios/Exercicios_Python_1/EX03.py
@@ -0,0 +1,5 @@
+numeros = list(range(0, 21))
+
+for numero in numeros:
+    if numero % 2 == 0:
+        print(numero)
